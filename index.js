7d9b5b26ab6baf6fc83ed420988acd1d27324c90
Z-Link-C <zachery.croft@gmail.com>
2026-03-10 12:45:51 -0400

    time to grade again

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your solution in this file!
-burgers = ['Hamburger', 'Cheeseburger'];
+let burgers = ['Hamburger', 'Cheeseburger'];
 var featuredDrink = 'Strawberry Milkshake';
 
 function addBurger() {
